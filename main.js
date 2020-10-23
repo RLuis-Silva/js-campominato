2476c2188b2a38d1268ba53fc0293c2794a99953
Luis Silva <s.ros.luis@gmail.com>
2020-10-23 15:38:55 +0200

    add function numRandom() + while + if

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log("funziona js");
+// console.log("funziona js");
+
+var bombaArray = [];
+var lunghezzaBombaArray = 5;
+var i = 1;
+
+
+
+while( i <= lunghezzaBombaArray){
+    var bomba = numRandom(1, 5);
+    console.log("numero generato: ", bomba);
+
+    if(!bombaArray.includes()){
+        bombaArray.push(bomba);
+        console.log("Aggiungo numero nell' array:", bombaArray);
+        
+    }
+    i++;
+
+}
+
+console.log("La mia array:", bombaArray);
+
+
+function numRandom(min, max){
+    return Math.floor(Math.random() * (max- min + 1) + min);
+}
